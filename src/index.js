2b621c723d78d0e57b39333e5b831a3c963a628a
William Cory <willcory10@gmail.com>
2018-01-20 00:31:21 -0500

    3:34 of Tutorial #3

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
-class NavBar extends React.Component{
-	render(){
-		return (
-			<div className='topnav'>
-				<a href='#home'>Home</a>
-				<a href='#account'>Account</a>
-				<a href='#help'>Help</a>
-			</div>
-			);
-	}
-}
+import Layout from './components/Layout.js'
 
-ReactDOM.render(< NavBar />,document.getElementById('app'));
+
+const app = document.getElementById('app');
+ReactDOM.render(< Layout />,app);
